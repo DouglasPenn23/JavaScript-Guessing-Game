e4719c4632a39da442c36e06dcbcde4b6aedef73
DouglasPenn23 <63472155+DouglasPenn23@users.noreply.github.com>
2021-03-09 13:10:57 -0500

    Comments

--- guessing-game.js
+++ guessing-game.js
@@ -7,9 +7,15 @@ const winningGuess= "7"
 let youWon = false;
 let numberOfTries= 0;
 
+// Here we have a function asking a user for input
+// => is shorthand to say you're making a function
+
 const askUserForInput= () =>{
     return prompt("Give me your best guess!")
 }
+
+// While you have not won & number of tries is less than 3
+// create a variable called userInput and call the function to ask the user for input
 
 while(!youWon && numberOfTries <3){
     userInput = askUserForInput()
@@ -23,7 +29,10 @@ if(userInput === winningGuess){
 else {
     alert("You've lost")
 }
+
 numberOfTries++
 console.log(numberOfTries)
+
 }
+
 alert("game over")
